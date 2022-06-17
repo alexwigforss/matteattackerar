f7eb000c5e91aff008f191c96e8cf4f47d333c7f
Alex Wigforss <alex.wigforss@gmail.com>
2022-06-17 20:46:57 +0200

    three factorys done ekuation todo

--- problemz.py
+++ problemz.py
@@ -2,25 +2,27 @@ from random import randint
 from fractions import Fraction as frac
 
 run = True
-# TODO Refaktorisera denna till en eller flera klass ist börja med sub/add kanske
+# TODO Ekvationer
 # TODO bygg om till återanvändbara klasser med parametrar MIN/MAX
 
-multiList = []
-divList = []
-
-for x in range(1,11):
-    for y in range(1,11):
-        multiList.append(x*y)
+def ListOfSubAdd(fr,to):
+    # global inList
+    subList = []
+    addList = []
+    for x in range(fr,to+1):
+        for y in range(fr,to+1):
+            subList.append(x-y)
+            addList.append(x+y)
+    subList = list(dict.fromkeys(subList))
+    return subList,addList
+subbs, adds = ListOfSubAdd()
 
 def ListOfMulti(fr,to):
-    # global inList
     multiList = []
-    # genererar en lista över alla multiplikationer möjliga med talen x och y
     for x in range(fr,to+1):
         for y in range(fr,to+1):
             multiList.append(x*y)
     noDupList = list(dict.fromkeys(multiList))
-    #inList = noDupList
     return noDupList
 multiList = ListOfMulti()
 
@@ -31,23 +33,64 @@ def ListofDivi(fr,to):
         for y in range(fr,to+1):
             z = frac(float(x/y)).limit_denominator(10)
             divlist.append(z)
-    divlist = list(dict.fromkeys(divlist))    # kastar den genom dict och tillbaka för att få bort alla dubletter
+    divlist = list(dict.fromkeys(divlist))
     return divlist
 divList = ListofDivi()
+
+# TODO UNDER CONSTRUCTION TODO
+def ListofEkvation(fr,to):
+    ekvlist = []
+    for x in range(fr,to+1):
+        for y in range(fr,to+1):
+            z = frac(float(x/y)).limit_denominator(10)
+            ekvlist.append(z)
+    ekvlist = list(dict.fromkeys(ekvlist))
+    return ekvlist
+ekvlist = ListofDivi()
+# TODO UNDER CONSTRUCTION TODO
 
 # Funktioner som returnerar tal från listorna med "lösningar"
 def getRandomMulti():
     r = multiList[randint(0,41)]
     return r
 
+def getRandomMulti(str,end):
+    r = multiList[randint(str,end)]
+    return r
+
 def getRandomDiv():
     r = divList[randint(0,41)]
     return r
 
-# TODO Bygg om så att listan kan skickas in utifrån och med dynamisk längd
-def getRandFromMulti(l):
+def getRandomDiv(str,end):
+    r = divList[randint(str,end)]
+    return r
+
+def getRandomMulti():
     r = multiList[randint(0,41)]
     return r
+
+def getRandomMulti(str,end):
+    r = multiList[randint(str,end)]
+    return r
+
+def getRandomSubAdd():
+    rsub = subbs[randint(0,41)]
+    radd = subbs[randint(0,41)]
+    return rsub, radd
+
+def getRandomSubAdd(str,end):
+    rsub = subbs[randint(str,end)]
+    radd = subbs[randint(str,end)]
+    return rsub, radd
+
+def getRandomEkv():
+    ekv = ekvlist[randint(0,41)]
+    return ekv
+
+def getRandomEkv(str,end):
+    ekv = ekvlist[randint(str,end)]
+    return ekv
 
 
 # while run:
