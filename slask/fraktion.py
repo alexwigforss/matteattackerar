b02b3b61486a14ecf0219f9b97737eacdb915381
Alex Wigforss <alex.wigforss@gmail.com>
2023-05-11 14:55:08 +0200

    unused code

---
+++ slask/fraktion.py
@@ -0,0 +1,16 @@
+from fractions import Fraction as frac
+
+reslist = []
+for x in range(1,11):
+    for y in range(1,11):
+        #reslist.append(float(x/y))
+        z = frac(float(x/y)).limit_denominator(10)
+        reslist.append(z)
+        # print(z,"\t",end=" ")
+    print()
+#print(reslist,len(reslist))
+print(len(reslist))
+print()
+shortlist = list(dict.fromkeys(reslist))
+
+print(shortlist)
